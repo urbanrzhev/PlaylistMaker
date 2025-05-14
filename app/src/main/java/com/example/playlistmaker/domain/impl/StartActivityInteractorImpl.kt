18423a4186c9d128c1046577b022fd4c34604d8a
urbanrzhev <aorus69reg@hotmail.com>
2025-05-14 14:56:20 +0300

    Update StartActivityInteractorImpl.kt

--- app/src/main/java/com/example/playlistmaker/domain/impl/StartActivityInteractorImpl.kt
+++ app/src/main/java/com/example/playlistmaker/domain/impl/StartActivityInteractorImpl.kt
@@ -4,13 +4,14 @@ import com.example.playlistmaker.domain.api.SharedPreferencesRepository
 import com.example.playlistmaker.domain.api.StartActivityInteractor
 
 class StartActivityInteractorImpl(private val sharedRepository: SharedPreferencesRepository):StartActivityInteractor {
+  val key = MyConstants. 
     override fun getStartActivity(
         consumer: StartActivityInteractor.BooleanConsumer
     ) {
-        consumer.consume(sharedRepository.getMediaPlayerLoadStartActivity())
+        consumer.consume(sharedRepository.getBoolean(key) /*getMediaPlayerLoadStartActivity()*/)
     }
 
-    override fun setStartActivity(keyData: Boolean) {
-        sharedRepository.setMediaPlayerLoadStartActivity(keyData)
+    override fun setStartActivity(data: Boolean) {
+        sharedRepository.setBoolean(key, data) /*setMediaPlayerLoadStartActivity(keyData)*/
     }
 }
