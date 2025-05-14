5a9460a9e979a1d20fde6176f51d1c3d24154646
urbanrzhev <aorus69reg@hotmail.com>
2025-05-14 14:19:40 +0300

    Update TracksSearchResponse.kt

--- app/src/main/java/com/example/playlistmaker/data/dto/TracksSearchResponse.kt
+++ app/src/main/java/com/example/playlistmaker/data/dto/TracksSearchResponse.kt
@@ -1,5 +1,5 @@
 package com.example.playlistmaker.data.dto
-
+mmm
 data class TracksSearchResponse(val searchType: String,
                                 val expression: String,
                                 val results: List<TrackDto>):Response()
