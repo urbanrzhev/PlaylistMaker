d062288417f101099d25d54199791b2f85a99fe7
urbanrzhev <aorus69reg@hotmail.com>
2025-05-14 15:57:52 +0300

    Update SharedPreferencesRepository2Impl.kt

--- app/src/main/java/com/example/playlistmaker/data/shared_preference/SharedPreferencesRepository2Impl.kt
+++ app/src/main/java/com/example/playlistmaker/data/shared_preference/SharedPreferencesRepository2Impl.kt
@@ -8,15 +8,20 @@ import com.example.playlistmaker.domain.models.Track
 
 class SharedPreferencesRepository2Impl(private val sharedPreferencesClient: SharedPreferencesClient):
     SharedPreferencesRepository2 {
-    override fun getMediaPlayerLoadStartActivity(dto:SharedPreferencesBooleanRequest): Boolean {
-        val result = sharedPreferencesClient.getBooleanRequest(dto)
+    override fun getBoolean/*getMediaPlayerLoadStartActivity*/(key:String): Boolean {
+        val result = sharedPreferencesClient.getBooleanRequest(SharedPreferencesBooleanRequest 
+                                                               key = key
+                                                              )
         return result.result?:false
     }
 
-    override fun setMediaPlayerLoadStartActivity(dto:SharedPreferencesBooleanRequest) {
-        sharedPreferencesClient.setBooleanData(dto)
+    override fun setBoolean/*setMediaPlayerLoadStartActivity*/(key:String, data:Boolean) {
+        sharedPreferencesClient.setBoolean(SharedPreferencesBooleanRequest 
+                                                               key = key, 
+                                               data = data
+                                                              ))
     }
-
+/*
     override fun getAppDarkTheme(dto:SharedPreferencesBooleanRequest): Boolean {
            val result = sharedPreferencesClient.getBooleanRequest(dto)
         return result.result?:false
@@ -25,10 +30,12 @@ class SharedPreferencesRepository2Impl(private val sharedPreferencesClient: Shar
     override fun setAppDarkTheme(dto:SharedPreferencesBooleanRequest) {
         sharedPreferencesClient.setBooleanData(dto) 
 
-    }
+    }*/
 
-    override fun getActiveTrackForMediaPlayer(dto:SharedPreferencesTrackRequest): Track{
-           val result = sharedPreferencesClient.getTrackRequest(dto)
+    override fun getTrack/*ActiveTrackForMediaPlayer*/(key:String): Track{
+           val result = sharedPreferencesClient.getTrackRequest(SharedPreferencesTrackRequest(
+               key = key
+           ) )
            return if(result != null){
                // time format
                 results as Track
@@ -36,12 +43,17 @@ class SharedPreferencesRepository2Impl(private val sharedPreferencesClient: Shar
         }
 
 
-    override fun setActiveTrackForMediaPlayer(dto:SharedPreferencesTrackRequest) {
-        sharedPreferencesClient.setTrackData(dto)
+    override fun setTrack/*setActiveTrackForMediaPlayer*/(key:String, data:Track) {
+        sharedPreferencesClient.setTrackData(SharedPreferencesTrackRequest(
+            key = key, 
+            data = data as TrackDto
+        ) )
     }
 
-    override fun getSearchHistoryTrackList(dto:SharedPreferencesTrackListRequest ): List<Track> {
-        val result = sharedPreferencesClient.getTrackListRequest(dto)
+    override fun getTrackList/*SearchHistoryTrackList*/(key:String ): List<Track> {
+        val result = sharedPreferencesClient.getTrackListRequest(SharedPreferencesTrackListRequest (
+            key = key
+        ) )
         // time format
         val res = result.map{
             Track(
@@ -61,12 +73,32 @@ class SharedPreferencesRepository2Impl(private val sharedPreferencesClient: Shar
         return emptyList()
     }
 
-    override fun setSearchHistoryTrackList(dto:SharedPreferencesTrackListRequest) {
-                 sharedPreferencesClient.setTrackListData(dto)
+    override fun setTrackList/*SearchHistoryTrackList*/(key:String, data:List<Track>) {
+                 sharedPreferencesClient.setTrackListData(SharedPreferencesTrackListRequest(
+                     key = key, 
+                     data = data. map{
+                         TrackDto(
+                        trackName = it.trackName,
+                        artistName = it.artistName,
+                //time
+                        trackTimeNormal = it.trackTimeNormal
+                        artworkUrl100 = it.artworkUrl100,
+                        previewUrl = it.previewUrl,
+                        collectionName = it.collectionName,
+                        releaseDate = it.releaseDate,
+                        primaryGenreName = it.primaryGenreName,
+                        country = it.country,
+                        trackId = it.trackId
+                    )
+                     } 
+                 ) )
     }
 
-    override fun clearSearchHistoryTrackList(dto:SharedPreferencesTrackListRequest) {
-   sharedPreferencesClient.clearTrackListData(dto)
+    override fun clear/*SearchHistory*/TrackList(key:String, del:Boolean) {
+   sharedPreferencesClient.clearTrackListData(SharedPreferencesTrackListRequest(
+       key = key, 
+       clear = del
+   ))
     }
 }
 
