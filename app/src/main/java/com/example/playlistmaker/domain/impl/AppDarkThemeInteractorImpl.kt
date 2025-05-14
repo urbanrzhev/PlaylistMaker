5979bca50e0acd0b7d226ee69fb17bf6bac828bf
urbanrzhev <aorus69reg@hotmail.com>
2025-05-14 15:01:30 +0300

    Update AppDarkThemeInteractorImpl.kt

--- app/src/main/java/com/example/playlistmaker/domain/impl/AppDarkThemeInteractorImpl.kt
+++ app/src/main/java/com/example/playlistmaker/domain/impl/AppDarkThemeInteractorImpl.kt
@@ -4,13 +4,14 @@ import com.example.playlistmaker.domain.api.AppDarkThemeInteractor
 import com.example.playlistmaker.domain.api.SharedPreferencesRepository
 
 class AppDarkThemeInteractorImpl(private val sharedRepository: SharedPreferencesRepository):AppDarkThemeInteractor {
+   val key = 
     override fun getAppDarkTheme(
         consumer: AppDarkThemeInteractor.BooleanConsumer
     ) {
-        consumer.consume(sharedRepository.getAppDarkTheme())
+        consumer.consume(sharedRepository.getBoolean())
     }
 
-    override fun setAppDarkTheme(keyData: Boolean) {
-        sharedRepository.setAppDarkTheme(keyData)
+    override fun setAppDarkTheme(data: Boolean) {
+        sharedRepository.setBoolean(key, data)
     }
 }
